1baf1982c6471a0220904c416c66d1d226fcf9fd
nubi\Mohanad <mohanad1213@live.com>
2017-05-02 14:58:00 +0300

    add mvc unit test code

---
+++ MasterNet/MasterNet.test/Controllers/MasterTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MasterNet.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestStack;
+using System.Threading.Tasks;
+
+namespace MasterNet.Controllers.Tests
+{
+    [TestClass()]
+    public class MasteTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+   
+          //  TestStack.FluentMVCTesting.ViewResultTest viewResultTest = new TestStack.FluentMVCTesting.ViewResultTest()
+
+        }
+    }
+}
---
+++ MasterNet/masterConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masterConsole
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args[0] == "mohanad")
+                return 0;
+            else
+                return 1;
+        }
+    }
+}
